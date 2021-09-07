e77aeba58b61e5412c5bf1600d2ef40dd7e6927d
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-09-07 20:46:36 +0530

    Started Data Structure(Destructuring Arrays)

---
+++ 09-Data Structures, Modern Operators and Strings/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const restaurant = {
+    name: "Garima's Kitchen",
+    Location: 'Cyber Hub, DLF Phase-3',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starteMenu: ['Focaccia', 'Bruchetta', 'Garlic Bread', 'Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function(starterIndex, mainIndex){
+        return [this.starteMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+};
+
+////Destructuring Arrays
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+console.log(a, arr);
+//Destructuring 
+const [x, ,y] = arr;
+console.log(x, y);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(restaurant.categories);
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Recieve 2 return values from the function through destructuring
+const [starter, mainCourse] = restaurant.order(2, 0)
+console.log(starter, mainCourse);
+//Nested array and destructuring
+const nested = [2, 4, [5, 6]];
+const [i, ,[j, k]] = nested;
+
+console.log(i, j, k);
+
+//Default value
+
+const [p=1, q=1, r=1] = [8,9];
+console.log(p, q, r);
