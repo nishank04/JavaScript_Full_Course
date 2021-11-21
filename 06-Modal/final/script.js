71775319d4b24685724c5c557caf638c016a6181
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2021-11-21 15:24:29 +0530

    Arrays-Bankist app

--- 06-Modal/final/script.js
+++ 06-Modal/final/script.js
@@ -1,49 +1,30 @@
  'use strict';
 
- class Vehicle {
-  constructor (name, maker, engine) {
-      this.name = name;
-      this. maker = maker;
-      this.engine = engine;
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
-  getDetails() {
-      return (`the name of the bike is ${this.name}.`)
-  }
-}
-
-//Making object with the help of constructor
-let bike1 = new Vehicle ('Ninja', 'Kawasaki', '998cc');
-
-console.log(bike1.name);
-console.log(bike1.maker);
-console.log(bike1.engine);
-
-
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.close-modal');
-// const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-// const openModal = function () {
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   // console.log(e.key);
-
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
+});
--- Arrays-Bankist/script.js
+++ Arrays-Bankist/script.js
@@ -64,10 +64,13 @@ const inputClosePin = document.querySelector('.form__input--pin');
 ///functions
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
+
+  const movs = sort ? movements.slice().sort((a, b) => a- b) : movements;
+
   
-  movements.forEach(function (mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -245,6 +248,14 @@ btnClose.addEventListener('click', function(e) {
         containerApp.style.opacity = 0;
     }
 })
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted
+})
+
 
 /*Interview Questions
 1. Event loop concepts(single threaded then how asycronous)
@@ -550,3 +561,53 @@ const overallBalance = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
+
+//Sorting Arrays
+
+//Strings
+const owners = ['Jatin', 'Akshay', 'Balwant', 'Raj', 'Saurabh']
+console.log(owners.sort())
+console.log(owners)
+
+//Numbers
+console.log(movements)
+
+//return < 0, A, B (keeping the order)
+//return > 0, A, B (switching the order)
+
+//Ascending
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+})
+// movements.sort((a, b) => a-b)
+console.log(movements)
+
+//Descending
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+})
+movements.sort((a, b) => b-a)
+console.log(movements)
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1,2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x)
+console.log(x.map(() => 5))
+//x.fill(1);
+//console.log(x)
+x.fill(1, 3, 5)
+console.log(x)
+
+arr1.fill(23, 2, 6)
+console.log(arr1)
+
+//Array.from
+const y = Array.from({length: 7}, ()=> 1)
+console.log(y)
+
+const z = Array.from({length: 7}, (cur, i)=> i + 1)
+console.log(z)
