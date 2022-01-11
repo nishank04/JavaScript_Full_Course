3d2a5f068aeed31676934a5e31542fc348a86a5f
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2022-01-11 20:40:33 +0530

    DOM elements bankist website

---
+++ 13-Advanced DOM and Events/script.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelector('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelector('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelector('.operations__content');
+
+////Modal Window
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function (e) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnsOpenModal.forEach(btn => btn.addEventListner('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+ ///Button Scrolling
+btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+
+    console.log(e.target.getBoundingClientRect());
+    console.log('Current scroll (x/y', window.pageXOffset, window.pageYOffset);
+
+    console.log(
+        'height/width viewport',
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth
+    );
+    section1.scrollIntoView({behavior: 'smooth'});
+});
+
+///Page Navigation
+document.querySelector('.nav__links').addEventListener('click', function(e){
+    e.preventDefault();
+
+    //Matching Strategy
+    if(e.target.classList.contains('nav__links')){
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    }
+});
+
+//Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operations__tab');
+
+    //Guard clause
+    if (!clicked) return;
+
+    //Remove active classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+    //Active Tab
+
+    clicked.classList.add('operations__tab--active');
+
+    //Activate content area
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add('operations__content--active');
+
+});
+
+///Menu fade animation
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
